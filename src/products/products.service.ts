ce5c1861a3e4208c3ba7d964b051e9ce6db1eacf
Geovanny Bucay <ebucayle@pichincha.com>
2024-04-23 21:51:51 -0500

    add validat product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -72,4 +72,24 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
     return product;
   }
+
+  async validateProducts(ids: number[]) {
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+    });
+
+    if (products.length !== ids.length) {
+      throw new RpcException({
+        message: 'Some products are not valid',
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+    return products;
+  }
 }
